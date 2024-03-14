f285664c915d7ff230eb17bdccc26e2c74e056ba
AyushSakpal <ayushsakpal2002@gmail.com>
2024-03-14 13:08:19 +0530

    update-stringBasicAssig.js

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,14 @@
+
+
+function stringBasics() {
+    var myDreamCompany = "TCS";
+    console.log(`My Dream Company is ${myDreamCompany}`);
+    var hobby1 = "Driving";
+    var hobby2 = "travelling";
+    var hobby3 = "Gameing";
+    console.log(`my hobbyies are ${hobby1},${hobby2},${hobby3}`);
+    console.log(`my hobby are ${hobby1} and its length is ${hobby1.length}`);
+    console.log(`my hobby are ${hobby2} and its length is ${hobby2.length}`);
+    console.log(`my hobbyies are ${hobby3} and its length is ${hobby3.length}`);
+}
+stringBasics();
