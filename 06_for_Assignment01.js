35cc185eb513ad33c8acaf638126c52895789607
AyushSakpal <ayushsakpal2002@gmail.com>
2024-03-20 15:12:16 +0530

    update-Assignment01

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,44 @@
+
+console.log("================== Numbers from 5 to 15 =======================");
+for (let i = 5; i <= 15; i++) {
+    console.log(i);
+}
+
+
+console.log("=================== Numbers from 50 to 40 ======================");
+for (let i = 50; i >= 40; i--) {
+    console.log(i);
+}
+
+
+console.log("=================== First 15 odd numbers ========================");
+for (let i = 1, count = 0; count < 15; i += 2) {
+    console.log(i);
+    count++;
+}
+
+
+console.log("==================== First 10 even numbers ======================");
+for (let i = 2, count = 0; count < 10; i += 2) {
+    console.log(i);
+    count++;
+}
+
+
+console.log("====================== Table of 5 ============================");
+for (let i = 1; i <= 6; i++) {
+    console.log(i * 5);
+}
+
+
+console.log("==================== Table of 10 =========================");
+for (let i = 1; i <= 6; i++) {
+    console.log(i * 10);
+}
+
+
+console.log("====================== Table of 10 in reverse order ============================");
+for (let i = 10; i >= 1; i--) {
+    console.log(i * 10);
+}
+
